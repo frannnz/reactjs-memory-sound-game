2c64da2533bd4ad09b80e6a20e8567c96dfcd300
franzcarl <franz@bisdahin.de>
2019-08-22 20:44:20 +0200

    Updates

---
+++ precache-manifest.839ad555ae4217a4e9aec958d8652ecc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48af827880ccbf2e9805817d4068d58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efc0ae803dd3fbdc9337",
+    "url": "./static/css/main.42e802f4.chunk.css"
+  },
+  {
+    "revision": "09dff200f7cf1983c019",
+    "url": "./static/js/2.bd839439.chunk.js"
+  },
+  {
+    "revision": "efc0ae803dd3fbdc9337",
+    "url": "./static/js/main.efd65c7b.chunk.js"
+  },
+  {
+    "revision": "d509f4b2d236d63cfa21",
+    "url": "./static/js/runtime~main.62ae1c1e.js"
+  },
+  {
+    "revision": "6f6148f9d2a9ee503c5b1c94090e69a6",
+    "url": "./static/media/applause.6f6148f9.mp3"
+  },
+  {
+    "revision": "74f63d789bc5b470ae9acc036a3c45e2",
+    "url": "./static/media/sound1.74f63d78.mp3"
+  },
+  {
+    "revision": "456f9c08a09ddfd821b9832149af312c",
+    "url": "./static/media/sound10.456f9c08.mp3"
+  },
+  {
+    "revision": "e5698cb2b6acfc9c51e0a32b6dd81b77",
+    "url": "./static/media/sound11.e5698cb2.mp3"
+  },
+  {
+    "revision": "dcd5dbab56a708ecf62fa7751e3200b4",
+    "url": "./static/media/sound12.dcd5dbab.mp3"
+  },
+  {
+    "revision": "ef3b1a1baeb4bd12205b651c88f4b3d3",
+    "url": "./static/media/sound2.ef3b1a1b.mp3"
+  },
+  {
+    "revision": "bb0c597d253743d45fa98a5fda6d3a2e",
+    "url": "./static/media/sound3.bb0c597d.mp3"
+  },
+  {
+    "revision": "bd402b2aee610b8d980615858700d208",
+    "url": "./static/media/sound4.bd402b2a.mp3"
+  },
+  {
+    "revision": "e7d46d345a7142133e263eb08a170579",
+    "url": "./static/media/sound5.e7d46d34.mp3"
+  },
+  {
+    "revision": "1ca84d55feb33da7abbcc03b7260f69d",
+    "url": "./static/media/sound6.1ca84d55.mp3"
+  },
+  {
+    "revision": "5b73154e3e3d6000d180ef095fdd666d",
+    "url": "./static/media/sound7.5b73154e.mp3"
+  },
+  {
+    "revision": "eef4d6c3e76a061753116ae75b9e886a",
+    "url": "./static/media/sound8.eef4d6c3.mp3"
+  },
+  {
+    "revision": "5754e58b6326feced2d8e7aed526240e",
+    "url": "./static/media/sound9.5754e58b.mp3"
+  }
+]);
